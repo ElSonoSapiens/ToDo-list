4163c9bc0795bac6609082a620552c09d63163d3
ElSonoSapiens <dihf.47@gmail.com>
2023-12-15 00:38:29 -0300

    todo-list 01

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,19 +1,18 @@
 import "./App.css";
-import img01 from "./img/img-01.png";
+import img from "./img/img.jpeg";
 import TaskList from "./components/TaskList.jsx";
 
 function App() {
 	return (
 		<div className="App">
 			<div className="taskApp">
-				<div className="logoContainer">
-					<img className="logo" src={img01} alt="logo de imagen" />
-				</div>
+				<div className="logoContainer"></div>
 				{/* Espacio */}
 				<div className="taskMainList">
 					<h1>Tasks</h1>
 					<TaskList />
 				</div>
+				<img className="logo" src={img} alt="logo de imagen" />
 			</div>
 		</div>
 	);
